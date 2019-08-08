758637afcf65adb271b664356e70f641aef29d0a
Hubert Śliwiński <contact@empty.codes>
2019-08-08 03:24:05 +0200

    Fixed algorithm to publishing dual offers

--- rss/description_builder.py
+++ rss/description_builder.py
@@ -2,24 +2,48 @@ from rss import timedelta_pl
 
 def description_builder(uri, known_searches, offer):
     description = ""
+    offer_type = {
+        "BUY_NOW": {"name": "Kup teraz!",
+                    "state": False},
+        "AUCTION": {"name": "Licytacja",
+                    "state": False}
+    }
     
     if offer["sellingMode"]["format"] == "BUY_NOW":
-            offer_type = "Kup teraz!"
+        offer_type["BUY_NOW"]["state"] = True
     else:
-        offer_type = "Licytacja"
+        offer_type["BUY_NOW"]["state"] = True
 
     if "fixedPrice" in offer["sellingMode"]:
         price = offer["sellingMode"]["price"]["amount"]
         fixed_price = offer["sellingMode"]["fixedPrice"]["amount"]
 
         if price != fixed_price:
-            offer_type = "Licytacja z możliwością Kup teraz!"
+            offer_type["AUCTION"]["state"] = True
 
-    description += "<div><strong>{} {} - {}</strong></div>".format(
-        offer["sellingMode"]["price"]["amount"],
-        offer["sellingMode"]["price"]["currency"],
-        offer_type
-    )
+    if offer_type["BUY_NOW"]["state"] and offer_type["AUCTION"]["state"]:
+        description += "<div><strong>{} {} - {}</strong></div>".format(
+            offer["sellingMode"]["price"]["amount"],
+            offer["sellingMode"]["price"]["currency"],
+            offer_type["AUCTION"]["name"]
+        )
+
+        description += "<div><strong>{} {} - {}</strong></div>".format(
+            offer["sellingMode"]["fixedPrice"]["amount"],
+            offer["sellingMode"]["fixedPrice"]["currency"],
+            offer_type["BUY_NOW"]["name"]
+        )
+
+    else:
+        for selling_mode in offer_type:
+            if offer_type[selling_mode]["state"] == True:
+                offer_type_name = offer_type[selling_mode]["name"]
+
+        description += "<div><strong>{} {} - {}</strong></div>".format(
+            offer["sellingMode"]["price"]["amount"],
+            offer["sellingMode"]["price"]["currency"],
+            offer_type_name
+        )
 
     if "amount" in offer["delivery"]["lowestPrice"]:
         if offer["delivery"]["lowestPrice"]["amount"] == "0.00":
