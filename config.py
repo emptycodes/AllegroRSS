529ba7b20aef349c54cceeaea850eddbd4ae67c5
Hubert Śliwiński <contact@empty.codes>
2019-08-11 16:51:17 +0200

    Add possibility of changing config/secrets by environmental variables

--- config.py
+++ config.py
@@ -1,4 +1,33 @@
 import yaml
+import os
+
+def load_env_vars(config_dict):
+    for var_name in config_dict:
+        env_var = os.environ.get(var_name.upper())
+
+        if env_var != None:
+            if config_dict[var_name] == True or\
+            config_dict[var_name] == False:
+
+                if env_var.upper() == "TRUE":
+                    env_var = True
+                
+                elif env_var.upper() == "FALSE":
+                    env_var = False
+                
+                elif env_var.isdigit() != False:
+                    if int(env_var) == 0:
+                        env_var = False
+                    else:
+                        env_var = True
+
+            else:
+                if env_var.isdigit() != False:
+                    env_var = int(env_var)
+            
+            config_dict[var_name] = env_var
+  
+    return config_dict
 
 class config():
     def read(self):
@@ -7,7 +36,16 @@ class config():
                 config = yaml.safe_load(f)
             except yaml.YAMLError as exc:
                 print(exc)
-    
+
+        for headers in config:
+            config[headers] = load_env_vars(config[headers])
+
+        with open("config.yaml", "w") as f:
+            try:
+                yaml.dump(config, f)
+            except yaml.YAMLError as exc:
+                print(exc)
+
         return config
 
 class secrets():
@@ -18,6 +56,15 @@ class secrets():
             except yaml.YAMLError as exc:
                 print(exc)
         
+        important_vars = {"client_id": None,
+                          "client_secret": None}
+
+        loaded_env_vars = load_env_vars(important_vars)
+        for loaded_env_var in loaded_env_vars:
+            if loaded_env_vars[loaded_env_var] != None:
+                secrets["secrets"][loaded_env_var] =\
+                    loaded_env_vars[loaded_env_var]
+
         return secrets
 
     def update(self, refreshed_tokens):
