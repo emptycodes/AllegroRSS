ff9239b228ad77d8969a544fad5c65e8de4bceee
Hubert Śliwiński <contact@empty.codes>
2019-08-15 18:23:54 +0200

    Response time improvement using the application/JSON header

--- allegro/get_web_filters.py
+++ allegro/get_web_filters.py
@@ -1,29 +1,26 @@
 import requests
-from bs4 import BeautifulSoup
-import json
+
 
 def get_web_filters(link):
-    r = requests.get(link)
-    bs = BeautifulSoup(r.content, 'html.parser')
+    r = requests.get(link, headers={
+        "Accept": "application/json"
+    })
 
-    scripts = bs.findAll("script", nonce=True)
-    for script in scripts:
-        if script.text[:34] == "window.__listing_FiltersStoreState":
-            data = json.loads(script.text[36:])
-    
+    data = r.json()
+
     chosen_filters = {}
-    for slot in data["slots"]:
-        for filters in slot["filters"]:
-            for filter_value in filters["filterValues"]:
-                if filter_value["selected"]:
-                    if not filters["type"] in ["NUMERIC", "TEXT", "LOCATION"]:
-                        if not filters["name"] in chosen_filters:
-                            chosen_filters[filters["name"]] = []
-                        chosen_filters[filters["name"]].append(filter_value["name"])
+    for filters in data["listing"]["filters"]:
+        filters_type = filters["filters"][0]["type"]
+        for option in filters["filters"][0]['filterValues']:
+            if option["selected"]:
+                if not filters_type in ["NUMERIC", "TEXT", "LOCATION"]:
+                    if not filters["name"] in chosen_filters:
+                        chosen_filters[filters["name"]] = []
+                    chosen_filters[filters["name"]].append(option["name"])
 
-                    else:
-                        if not filters["name"] in chosen_filters:
-                            chosen_filters[filters["name"]] = {}
-                        chosen_filters[filters["name"]][filter_value["name"]] = filter_value["value"]
+                else:
+                    if not filters["name"] in chosen_filters:
+                        chosen_filters[filters["name"]] = {}
+                    chosen_filters[filters["name"]][option["name"]] = option["value"]
 
     return chosen_filters
