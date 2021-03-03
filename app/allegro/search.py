ab676cf7d3ed40f86232f8ad6b26ec976cdcbc6c
emptycodes <contact@empty.codes>
2021-03-03 01:57:52 +0100

    allegro/search: Set not default User Agent header

--- app/allegro/search.py
+++ app/allegro/search.py
@@ -4,7 +4,10 @@ import os.path
 
 TRUSTED_PATHS = ["/listing", "/kategoria"]
 
-HEADERS = {"Accept": "application/json"}
+HEADERS = {
+        "User-Agent": "Mozilla/5.0 (Windows NT 6.1; WOW64; rv:54.0) Gecko/20100101 Firefox/54.0",
+        "Accept": "application/json",
+}
 
 
 class NotTrustedAllegroPath(Exception):
